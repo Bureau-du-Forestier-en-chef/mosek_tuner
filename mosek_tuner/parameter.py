1023361a0b2df8ceca332e795c62b76d97fb059d
gcyr <Guillaume.Cyr@fec.gouv.qc.ca>
2024-04-23 08:45:54 -0400

    Comment header error

--- mosek_tuner/parameter.py
+++ mosek_tuner/parameter.py
@@ -1,9 +1,9 @@
-/*
+'''
 Copyright (c) 2023 Gouvernement du Québec
 
 SPDX-License-Identifier: LiLiQ-R-1.1
 License-Filename: LICENSES/EN/LiLiQ-R11unicode.txt
-*/
+'''
 
 from __future__ import annotations
 from abc import ABC, abstractmethod
